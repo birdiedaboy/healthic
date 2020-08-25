39e25278e7b9465bb689f2cc1d20553f8dfabe2a
birdiedaboy <abraham.esparza.eb@gmail.com>
2020-08-24 20:16:24 -0500

    paagregue equipo y sus foncionalidades de tab y modal

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-function openNav() {
-  document.getElementById("myNav").style.height = "100%";
-}
-
-function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
