d5cd5fdefe06d271af10d95c1b987322ccdf41d6
birdiedaboy <abraham.esparza.eb@gmail.com>
2020-08-31 12:53:38 -0500

    imagenes de los demas sliders optimizadas y navbar shrink

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ $(document).ready(function() {
     }
   });
 });
+
+$(document).ready(function() {
+  $(window).on("scroll", function() {
+    if ($(window).scrollTop() >= 350) {
+      $(".navbar-brand-imagen").addClass("enmobil");
+    } else {
+      $(".navbar-brand-imagen").removeClass("enmobil");
+    }
+  });
+});
+$(document).ready(function() {
+  $(window).on("scroll", function() {
+    if ($(window).scrollTop() >= 350) {
+      $(".nav-link").addClass("colorlink");
+    } else {
+      $(".nav-link").removeClass("colorlink");
+    }
+  });
+});
