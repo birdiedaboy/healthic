0de4febfaebc3e7ee999c975bd0071a8decf22ba
birdiedaboy <abraham.esparza.eb@gmail.com>
2020-08-31 11:09:45 -0500

    optimizando fotos de slider home y agregando tipografia de proyecto

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+$(document).ready(function() {
+  $(window).on("scroll", function() {
+    if ($(window).scrollTop() >= 350) {
+      $(".navbar").addClass("compressed");
+    } else {
+      $(".navbar").removeClass("compressed");
+    }
+  });
+});
